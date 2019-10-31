55ea00b5d81f84db4d226b86c7a107ef9a1f5d33
Masa (Mark) Fukui <fkmkfkmasum41@gmail.com>
2019-10-31 17:14:25 +0900

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.metacritic.com/browse/games/release-date/available/ps4/date'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+     (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
+
+response = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+# titles = soup.find_all("div", {"class":"basic_stat product_title"})
+titles = soup.select("div.basic_stat.product_title > a")
+
+for title in titles:
+    print(title.text)
+
