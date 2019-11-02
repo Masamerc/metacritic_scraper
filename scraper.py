90f892f409583af890a77b801800d44809739e86
Masa Fukui <fkmkfkmasum41@gmail.com>
2019-11-02 21:44:50 +0900

    better code

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = 'https://www.metacritic.com/browse/games/release-date/available/ps4/date'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
@@ -11,10 +12,21 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 games = soup.find_all("div", {"class":"product_wrap"})[:-3]
 
-# titles = soup.find_all("div", {"class":"basic_stat product_title"})
-# titles = soup.select("div.basic_stat.product_title > a")
+
+game_data = []
+for game in games:
+     title = game.find("div", {"class":"basic_stat product_title"})
+     for tag in game.find_all("li", {"class":"stat release_date"}):
+          release_date = tag.find("span", {"class":"data"})
+
+     pair = {
+          "title":title.text.strip(),
+          "release_date":release_date.text.strip()
+     }
+     game_data.append(pair)
 
 # for title in titles:
 #     print(title.text)
 
-print(games[-1])
+with open("data.json", "w") as f:
+     json.dump(game_data, f, indent=2)
