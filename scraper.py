9e77e56ff9463bfd30804fa89f3ec06f541f463c
Masa Fukui <fkmkfkmasum41@gmail.com>
2019-11-10 16:39:08 +0900

    Added mail functionality

--- scraper.py
+++ scraper.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from data_wrangler import DataHelper
+from email_data import send_email
+import datetime 
+
+
 
 url = 'https://www.metacritic.com/browse/games/release-date/available/ps4/date'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
@@ -33,3 +38,19 @@ for game in games:
 
 with open("data.json", "w") as f:
      json.dump(game_data, f, indent=2)
+
+
+dh = DataHelper
+
+test_data = dh.load_and_clean()
+complete_data = dh.get_complete_data(test_data)
+
+o_best = dh.store_overall_best(complete_data)
+u_best = dh.store_users_best(complete_data)
+c_best = dh.store_critic_best(complete_data)
+contr_good = dh.store_controverial_good(complete_data)
+contr_bad = dh.store_controverial_bad(complete_data)
+
+
+send_email(subject=f"Metacritic Scraper {datetime.date.today()}", content=o_best + "\n" + u_best\
+    + "\n" + c_best + "\n" + contr_good + "\n" + contr_bad)
--- test_data.py
+++ test_data.py
@@ -1,9 +1,10 @@
 from data_wrangler import DataHelper
-
+from email_data import send_email
+import datetime 
 dh = DataHelper
 
 test_data = dh.load_and_clean()
-complete_data = dh.get_complete_data(test_data, save_csv=True)
+complete_data = dh.get_complete_data(test_data)
 
 o_best = dh.store_overall_best(complete_data)
 u_best = dh.store_users_best(complete_data)
@@ -11,4 +12,6 @@ c_best = dh.store_critic_best(complete_data)
 contr_good = dh.store_controverial_good(complete_data)
 contr_bad = dh.store_controverial_bad(complete_data)
 
-# print(contr_bad)
+
+send_email(subject=f"Metacritic Scraper {datetime.date.today()}", content=o_best + "\n" + u_best\
+    + "\n" + c_best + "\n" + contr_good + "\n" + contr_bad)
